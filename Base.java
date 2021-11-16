7caa87ab743ef2afbd8202f983b0db65df04557b
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2021-11-16 16:02:44 +0900

    Update Base.java

--- Base.java
+++ Base.java
@@ -1,16 +1,13 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
-
 class TimeRunnable implements Runnable{
 	private JLabel timerLable;
-	
+	public int n = 0;
 	public TimeRunnable(JLabel timerLabel) {
 		this.timerLable = timerLabel;
 	}
 	public void run() {
-		int n = 0;
 		while(true) {
 			timerLable.setText(Integer.toString(n));
 			n++;
@@ -31,21 +28,29 @@ public class Base extends JFrame{
 	private JButton [] button = new JButton[10];
 	private JLabel clearlabel;
 	private JLabel timerLabel = new JLabel();
+	Container c= getContentPane();
 	private int index = 0;
 	private int [] getX = new int[10];
 	private int [] getY = new int[10];
-	
+	private int [] getNumber = new int[10];
 	public Base() {
 		this.setTitle("Base");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(getOwner());
-		Container c= getContentPane();
+		timerLabel.setFont(new Font("Gothic", Font.ITALIC, 40));
 		c.setLayout(null);
 		clearlabel = new JLabel("Clear!!");
 		c.add(clearlabel);
+		c.add(timerLabel);
+		c.add(restart);
+		restart.addActionListener(new MyActionListener());
 		clearlabel.setBounds(180,120,100,100);
 		clearlabel.setVisible(false);
-		c.setBackground(Color.WHITE);
+		restart.setBounds(235, 200, 100, 100);
+		restart.setVisible(false);
+		c.setBackground(Color.BLACK);
+		timerLabel.setForeground(Color.WHITE);
+		timerLabel.setBounds(270,0,50,50);
 		TimeRunnable runnable = new TimeRunnable(timerLabel);
 		Thread th = new Thread(runnable);
 		for(int i = 0; i<label.length;i++) {
@@ -54,41 +59,76 @@ public class Base extends JFrame{
 			getX[i] = x;
 			getY[i] = y;
 			label[i] = new JLabel(Integer.toString(i));
-			label[i].setBounds(x,y,20,20);
-			label[i].setForeground(Color.BLACK);
-			c.add(label[i]); 
-			label[i].setVisible(true);
-			System.out.println(timerLabel);
+			label[i].setBounds(x,y,40,40);
+			label[i].setForeground(Color.WHITE);
+			c.add(label[i]);
 			
 		}
 		for(int i = 0; i<button.length;i++) {
-			button[i] = new JButton();
+			button[i] = new JButton(""+i);
 			button[i].setBounds(getX[i], getY[i], 20,20);
 			button[i].addActionListener(new MyActionListener());
-			button[i] = new JButton(Integer.toString(i));
+			button[i].setForeground(new Color(0,0,0,0));
+			button[i].setBackground(Color.WHITE);
+			button[i].setBorderPainted(false);
 			c.add(button[i]);
-			if(button[i].getText().equals(i)) {
-				index++;
-				if(index==10) {
-					c.setBackground(Color.GRAY);
-					clearlabel.setVisible(true);
-					label[index].setVisible(true);
-				}
-			}
+			button[i].setVisible(false);
 			
 		}
 		setSize(600,600);
 		setVisible(true);
 		
 		th.start();
+		try {
+			for(int i = 0; i<label.length;i++) {
+				label[i].setVisible(true);
+			}
+			th.sleep(3000);
+			for(int i = 0; i<label.length;i++) {
+				label[i].setVisible(false);
+			}
+			for(int i =0;i<button.length;i++) {
+				button[i].setVisible(true);
+			}
+		}
+		catch(InterruptedException e) {
+			return;
+		}
 	}
 	private class MyActionListener implements ActionListener{
 		
 		public void actionPerformed(ActionEvent e) {
+			JButton b = (JButton)e.getSource();
 			for(int i = 0; i<button.length; i++) {
 				if(e.getSource() == button[i]) {
 					button[i].setVisible(false);
 				}
+				
+			}
+			if(b.getText().equals("Restart")) {
+				new Base();
+				dispose();
+			}
+			
+			else if(b.getText().equals(Integer.toString(index))) {
+				label[index].setVisible(true);
+				index++;
+				if(index == 10) {
+					for(int i =0; i<label.length;i++) {
+						label[i].setVisible(false);
+					}
+					c.setBackground(Color.GRAY);
+					clearlabel.setVisible(true);
+				}
+			}
+			else {
+				for(int i=0; i<button.length;i++) {
+					button[i].setVisible(false);
+				}
+				for(int i =0;i<label.length;i++) {
+					label[i].setVisible(false);
+				}
+				restart.setVisible(true);
 			}
 		}
 	}
